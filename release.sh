ed63789b2d1bedc6658780287aa44cf82e0955ba
David Brooks <d.brooks@auckland.ac.nz>
2024-09-07 10:10:58 +1200

    Stash repo as needed in release script.

--- release.sh
+++ release.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
-git stash
+clean=`git status | grep -q "nothing to commit"`
+if (( !clean )); then
+    git stash -u
+fi
+
 poetry build -f wheel
 
 git push origin
 git push origin v$1
 gh release create v$1 --verify-tag --title "Release $1" --notes ""
 gh release upload v$1 dist/flatmapknowledge-$1-py3-none-any.whl
-git stash pop --quiet
+
+if (( !clean )); then
+    git stash pop --quiet
+fi
